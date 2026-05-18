218f5ddbfbae2d24635cc8a382200b956038f0f1
Alex <56201083@qq.com>
2026-05-18 22:26:58 +0800

    修改启动脚本

--- run_service_single_gpu.sh
+++ run_service_single_gpu.sh
@@ -10,16 +10,14 @@ MODEL_PATH="./models/LiveAct"
 
 USE_CHANNELS_LAST_3D=1 CUDA_VISIBLE_DEVICES=0 \
 torchrun --nproc_per_node=1 --master_port=$(shuf -n 1 -i 10000-65535)  \
-    demo.py \
+    liveact_service.py \
     --size 416*720 \
     --ckpt_dir "$MODEL_PATH" \
     --wav2vec_dir ./models/chinese-wav2vec2-base \
     --video_save_path ./generated_videos \
     --fp8_kv_cache \
-#    --block_offload \
-#    --t5_cpu
-#    --fps 20 \
+    --block_offload \
+    --t5_cpu \
+    --fps 20
 #    --dura_print \
-#    --input_json examples/example.json \
 #    --steam_audio
-#    --video_save_path ./generated_videos
